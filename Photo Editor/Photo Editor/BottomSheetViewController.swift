994a3ac4755bd5d1c7faf43603eeea7ee61e527e
Mohamed Hamed <hamed.fci@gmail.com>
2017-04-23 19:56:40 +0200

    Added collection view to bottom sheet

--- Photo Editor/Photo Editor/BottomSheetViewController.swift
+++ Photo Editor/Photo Editor/BottomSheetViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class BottomSheetViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var holdView: UIView!
+    @IBOutlet weak var collectioView: UICollectionView!
 
     
     let fullView: CGFloat = 100 // remainder of screen height
@@ -20,12 +21,17 @@ class BottomSheetViewController: UIViewController, UIGestureRecognizerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectioView.delegate = self
+        collectioView.dataSource = self
+        
         holdView.layer.cornerRadius = 3
         let gesture = UIPanGestureRecognizer.init(target: self, action: #selector(BottomSheetViewController.panGesture))
         gesture.delegate = self
         view.addGestureRecognizer(gesture)
         
-       
+        let nib = UINib(nibName: "StickerCollectionViewCell", bundle: nil)
+        collectioView.register(nib, forCellWithReuseIdentifier: "StickerCollectionViewCell")
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -92,7 +98,45 @@ class BottomSheetViewController: UIViewController, UIGestureRecognizerDelegate {
 
 }
 
-//extension BottomSheetViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-// 
-//}
+// MARK: - UICollectionViewDataSource
+extension BottomSheetViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        layout.minimumLineSpacing = 20;
+        layout.minimumInteritemSpacing = 20;
+        var screenSize = UIScreen.main.bounds.size
+        screenSize.width = (CGFloat) ((screenSize.width - 30) / 3.0);
+        screenSize.height = 100;
+        return screenSize;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //The magic here
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "StickerCollectionViewCell"
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! StickerCollectionViewCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+}
 
