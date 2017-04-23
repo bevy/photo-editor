412595b44fa9e31b9601acf8114e75667045bd4e
Mohamed Hamed <hamed.fci@gmail.com>
2017-04-23 12:27:01 +0200

    Initial commit

---
+++ Photo Editor/Photo Editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Editor
+//
+//  Created by Mohamed Hamed on 4/23/17.
+//  Copyright © 2017 Mohamed Hamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
