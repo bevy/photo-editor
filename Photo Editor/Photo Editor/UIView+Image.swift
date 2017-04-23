20ebc32f656d99c176b1816aa8f8bece69051c86
Mohamed Hamed <hamed.fci@gmail.com>
2017-04-23 14:15:03 +0200

    Added controls

---
+++ Photo Editor/Photo Editor/UIView+Image.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Image.swift
+//  Photo Editor
+//
+//  Created by Mohamed Hamed on 4/23/17.
+//  Copyright © 2017 Mohamed Hamed. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func toImage() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0)
+        self.drawHierarchy(in: self.bounds, afterScreenUpdates: false)
+        let snapshotImageFromMyView = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return snapshotImageFromMyView!
+    }
+}
--- Photo Editor/Photo Editor/ViewController.swift
+++ Photo Editor/Photo Editor/ViewController.swift
@@ -10,16 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //
+    var lastPoint:CGPoint!
+    var isSwiping:Bool!
+    var red:CGFloat!
+    var green:CGFloat!
+    var blue:CGFloat!
+    //
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func saveImage(_ sender: AnyObject) {
+        if self.imageView.image == nil{
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(self.imageView.toImage(),self, #selector(ViewController.image(_:withPotentialError:contextInfo:)), nil)
+    }
+    @IBAction func undoDrawing(_ sender: AnyObject) {
+        self.imageView.image = UIImage(named: "img.jpg")
+        let rect = CGRect(x: 200, y: 200, width: 100, height: 100)
+        let tempView = UIView(frame: rect)
+        tempView.backgroundColor = UIColor.red
+        self.imageView.addSubview(tempView)
+    }
+    func image(_ image: UIImage, withPotentialError error: NSErrorPointer, contextInfo: UnsafeRawPointer) {
+        UIAlertView(title: nil, message: "Image successfully saved to Photos library", delegate: nil, cancelButtonTitle: "Dismiss").show()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.image = UIImage(named: "img.jpg")
         // Do any additional setup after loading the view, typically from a nib.
+        red   = (0.0/255.0)
+        green = (0.0/255.0)
+        blue  = (0.0/255.0)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Touch events
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?){
+        isSwiping    = false
+        if let touch = touches.first{
+            lastPoint = touch.location(in: imageView)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>,
+                               with event: UIEvent?){
+        
+        isSwiping = true;
+        if let touch = touches.first{
+            let currentPoint = touch.location(in: imageView)
+            UIGraphicsBeginImageContext(self.imageView.frame.size)
+            self.imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
+            UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+            UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: currentPoint.x, y: currentPoint.y))
+            UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+            UIGraphicsGetCurrentContext()?.setLineWidth(5.0)
+            UIGraphicsGetCurrentContext()?.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
+            UIGraphicsGetCurrentContext()?.strokePath()
+            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>,
+                               with event: UIEvent?){
+        if(!isSwiping) {
+            // This is a single touch, draw a point
+            UIGraphicsBeginImageContext(self.imageView.frame.size)
+            self.imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
+            UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+            UIGraphicsGetCurrentContext()?.setLineWidth(9.0)
+            UIGraphicsGetCurrentContext()?.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
+            UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+            UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+            UIGraphicsGetCurrentContext()?.strokePath()
+            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+        }
+    }
+    
+    
 }
 
