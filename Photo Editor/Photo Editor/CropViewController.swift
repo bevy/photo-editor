dec8e44c6a100590688aee104b4713519d34c4c7
Thomas Torbik (ttorbik) <thomastorbik@gmail.com>
2018-11-30 19:08:41 -0600

    Update to Swift 4.2

--- Photo Editor/Photo Editor/CropViewController.swift
+++ Photo Editor/Photo Editor/CropViewController.swift
@@ -127,11 +127,11 @@ open class CropViewController: UIViewController {
         cropView?.resetCropRectAnimated(animated)
     }
     
-    func cancel(_ sender: UIBarButtonItem) {
+    @objc func cancel(_ sender: UIBarButtonItem) {
         delegate?.cropViewControllerDidCancel(self)
     }
     
-    func done(_ sender: UIBarButtonItem) {
+    @objc func done(_ sender: UIBarButtonItem) {
         if let image = cropView?.croppedImage {
             guard let rotation = cropView?.rotation else {
                 return
@@ -143,7 +143,7 @@ open class CropViewController: UIViewController {
         }
     }
     
-    func constrain(_ sender: UIBarButtonItem) {
+    @objc func constrain(_ sender: UIBarButtonItem) {
         let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         let original = UIAlertAction(title: "Original", style: .default) { [unowned self] action in
             guard let image = self.cropView?.image else {
