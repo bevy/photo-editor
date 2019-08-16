7fdc5aa40b0b6f379fcff8f0d5a3240144d4e0cf
Mohamed Hamed <mohamed.hamed.ibrahem@gmail.com>
2019-08-16 12:10:50 +0200

    fixed memory leaks because of retain cycles

--- Example/Pods/iOSPhotoEditor/Photo Editor/Photo Editor/Protocols.swift
+++ Example/Pods/iOSPhotoEditor/Photo Editor/Photo Editor/Protocols.swift
@@ -14,7 +14,7 @@ import UIKit
  - stickersViewDidDisappear
  */
 
-public protocol PhotoEditorDelegate {
+public protocol PhotoEditorDelegate: class {
     /**
      - Parameter image: edited Image
      */
@@ -31,7 +31,7 @@ public protocol PhotoEditorDelegate {
  - didSelectImage
  - stickersViewDidDisappear
  */
-protocol StickersViewControllerDelegate {
+protocol StickersViewControllerDelegate: class {
     /**
      - Parameter view: selected view from StickersViewController
      */
@@ -49,6 +49,6 @@ protocol StickersViewControllerDelegate {
 /**
  - didSelectColor
  */
-protocol ColorDelegate {
+protocol ColorDelegate: class {
     func didSelectColor(color: UIColor)
 }
